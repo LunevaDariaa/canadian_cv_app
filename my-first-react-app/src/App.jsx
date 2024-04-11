6d996143db614d101c72238606106b62e6dbaeae
Daria Luneva <daria.safri@gmail.com>
2024-04-10 20:01:14 -0400

    Started to create main content, byulding components and styling

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,5 +1,51 @@
 import { useState } from "react";
 
+function Button() {
+  return <button className="bn3637 bn37">Clear</button>;
+}
+
 export default function App() {
-  return <div>Hello, World</div>;
+  return (
+    <div className="app">
+      <FormSidebar />
+      <Resume />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Content</h1>
+      <Button />
+    </div>
+  );
+}
+
+function FormSidebar() {
+  return (
+    <div className="form-sidebar">
+      <Header />
+    </div>
+  );
+}
+
+function Resume() {
+  return <div className="resume"></div>;
+}
+
+function PersonalInfo() {
+  return <div></div>;
+}
+
+function Experience() {
+  return <div></div>;
+}
+
+function Education() {
+  return <div></div>;
+}
+
+function Skills() {
+  return <div></div>;
 }
