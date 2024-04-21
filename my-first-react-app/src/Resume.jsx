160407a6d545dd0291f0aeef50ed6bd2683f2772
Daria Luneva <daria.safri@gmail.com>
2024-04-21 10:54:07 -0400

    Started to style the final look of resume

--- my-first-react-app/src/Resume.jsx
+++ my-first-react-app/src/Resume.jsx
@@ -49,11 +49,26 @@ export function Resume({
 }
 
 function SchoolResume({ school }) {
+  const {
+    degree,
+    startDateEducation,
+    endDateEducation,
+    schoolLocation,
+    schoolName,
+  } = school;
+
+  const startYear = new Date(startDateEducation).getFullYear();
+  const startMonth = new Date(startDateEducation).getMonth();
+  const endYear = new Date(endDateEducation).getFullYear();
+  const endMonth = new Date(endDateEducation).getMonth();
+
   return (
-    <div>
-      {school.isSeen && "True"}
-      {school.id}
-      {school.schoolName}
+    <div className="education">
+      <div className="main-info-education-resume">
+        <div>{degree}</div>
+        <div className="heading-dates-education-resume">{`${startYear}/${startMonth} - ${endYear}/${endMonth}`}</div>
+      </div>
+      <div>{`${schoolName} , ${schoolLocation}`}</div>
     </div>
   );
 }
